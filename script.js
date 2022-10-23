8c42a16809dcdb536dbc73c7e418c47dae2fb80a
teshughoul <crgabrielnj@gmail.com>
2022-10-23 10:55:53 -0300

    definir direcoes de movimentacao e impedir que a cobrinha suma da tela

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//VARIÁVEIS//
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -6,13 +7,17 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
+
 let direction = "right";
 
+//BACKGROUND//
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+//CRIAR COBRINHA//
 function criarCobrinha() {
     for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
@@ -20,27 +25,49 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+//CONTROLES DE MOVIMENTAÇÃO//
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
+//AO INICIAR JOGO//
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    
     criarBG();
     criarCobrinha();
 
+    //PONTO DE PARTIDA//
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeY += box;
+    //LÓGICA DE MOVIMENTAÇÃO COORDENADA//
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     snake.pop();
 
+    //"CABEÇA DA COBRINHA"//
     let newHead = {
         x: snakeX,
         y: snakeY
     }
-
+    
     snake.unshift(newHead);
 
 }
 
+//START//
 let jogo = setInterval(iniciarJogo, 100);
+
